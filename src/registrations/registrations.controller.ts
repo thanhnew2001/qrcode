bff2f4904a2a7f9acd0aba1c8c559da2e609a750
thanhnew2001 <thanh.hispvietnam@gmail.com>
2021-12-17 19:27:40 +0700

    Update registrations.controller.ts

--- src/registrations/registrations.controller.ts
+++ src/registrations/registrations.controller.ts
@@ -36,8 +36,8 @@ export class RegistrationsController {
   verfiy(@Param('id') id: string) {
     let idn =  parseInt(id)
     if (idn%2 ===0)
-    return JSON.parse(`{"valid": "Yes"}`)
+    return JSON.parse(`{"valid": "Yes", "name: "Dinh Ngoc Minh", "type": "Staff", "id": "v175923"}`)
     else 
-    return JSON.parse(`{"valid": "No"}`)
+    return JSON.parse(`{"valid": "No", "name: "Hoang Ngoc Le", "type": "Student", "id": "s175923"}`)
   }
 }
